f60d96ce1e478f70b0f10389b8386069aebe754f
Luca Cavazzini <lucava9526@gmail.com>
2023-03-01 14:29:08 +0100

    working with one file

--- video_downloader_project/all_variables_path_using.py
+++ video_downloader_project/all_variables_path_using.py
@@ -17,16 +17,17 @@ def set_desktop():
 def click(a, b, c, d, e):
             try:
                 try:
+                    full_path = (os.path.join(c, d))
                     all_input = a.get("1.0", "end")
                     result=all_input.split("https")
                     #adding som eminus two here as maybe this is getting in the way 
                     updated_result = ["https"+i[:-2] for i in result if len(i)>5]
                     link_list_len = len( updated_result)
                     b.config(text="convertion output" + "/n" + "videos links detected :" + str(link_list_len))
-                    
-                    if not os.path.exists(os.path.join(c, d)):
-                        os.makedirs(os.path.join(c, d))
+                    if not os.path.exists(full_path):
+                        os.makedirs(full_path)
                         messagebox.showinfo("folder info", "download folder created")
+                    
                     else:
                         try:
                             pass
@@ -46,10 +47,10 @@ def click(a, b, c, d, e):
                 e.delete('1.0', tk.END)
                 #updating the text boxes with the values split as it is a list
                 input_b = [a.insert("end", i + "\n") for i in updated_result]
-                output_b = [e.insert("end", i + "\n") for i in updated_result]
+                output_b = [i for i in updated_result]
                     
                 # should be working up until now 
-                
+                print("output_b, updated_result --------------->", output_b, updated_result)
                 
                 
                 
@@ -61,13 +62,13 @@ def click(a, b, c, d, e):
                 try:
                     for link in updated_result:
                         #need to fix this as we get no 
-                        download_trial = Download(link[:-2],(os.path.join(c, d)))
+                        download_trial = Download(link[:-2],(full_path + "\\"))
                         
                         if  download_trial:
-                            #pop seems to be working now
+                            #pop seems to be working now, but should be added in the function
                             index_in_list = updated_result.index(link)
                             updated_result.pop(index_in_list)
-                            update_download_convert("Downloaded", f'{link}', updated_result, d)  
+                            update_download_convert("-- Downloaded", link, output_b, e)  
                         else: next
         #i should have the list all converted now
                 except Exception as e:
@@ -77,20 +78,23 @@ def click(a, b, c, d, e):
                             messagebox.showinfo(message=str(e) + str(exc_type)  + str(fname)+ str(exc_tb.tb_lineno))
                 
                 
-                while checking_if_mp4_left(os.path.join(c, d)):
-                    for filename in os.scandir(os.path.join(c, d)):
+                while checking_if_mp4_left((full_path + "\\")):
+                    for filename in os.scandir((full_path + "\\")):
                         if filename.is_file() and ".mp4" in filename.name:
                             try:
+                                print("filename", filename)
+                                folder_to_iterate = os.listdir((full_path + "\\"))
                             #transforms the file 
                                 clip = VideoFileClip(filename.path)
                 #writes the corrected file
-                                clip.audio.write_audiofile(os.path.join(c, d) + filename.name[:-4] + ".mp3")
+                                clip.audio.write_audiofile((full_path + "\\") + filename.name[:-4] + ".mp3")
             #cleaning out mp4 file
                                 clip.close()
+                                update_download_convert("--  Dwnloaded Converted", filename.name, folder_to_iterate,e)
                     #removing the file using filename -path should be there
                                 os.remove(filename)
                     #need to rethink the lists here, with the index taken into consideration to change the mput boxes 
-                                update_download_convert("Converted", filename)
+                                
                             except Exception as e:
                                 exc_type, exc_obj, exc_tb = sys.exc_info()
                                 fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
@@ -139,11 +143,11 @@ def Download(link, path):
             #using download_result as a flag
             return download_result
         
-def update_download_convert(string, filename, output_b, output_box):
+def update_download_convert(string, filename, list_from_output, output_box):
             #there is something wrong here, code needs some fixing
             #how can i have anything in the list if i popped it ? the variable has been modified 
-            print("all variables in the arguments ", string, filename, output_b, output_box)
-            index = output_b.index(filename)
-            output_b[index] = filename + string
-            [output_box.insert("end", i + "\n") for i in output_b]
+            print("all variables in the arguments ", string, filename, list_from_output, output_box)
+            index =list_from_output.index(filename)
+            list_from_output[index] = str(filename) + string
+            [output_box.insert("end", i + "\n") for i in list_from_output]
         
